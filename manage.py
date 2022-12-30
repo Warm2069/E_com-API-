e288bdcefefde2ea7076e9c00aec3f99214ea3dc
Warm2069 <114212343+Warm2069@users.noreply.github.com>
2022-12-30 16:44:16 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 import dotenv
 def main():
-    dotenv.load_dotenv()
+    dotenv.read_dotenv()
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_commerce.settings')
     try:
